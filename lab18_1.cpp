e39bda5700bb5b6dbb5f67b7bbdf0328eaffaf8d
pattarapon <honamiyue@gmail.com>
2024-02-12 11:50:43 +0700

    complete

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,7 +5,22 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2){
+	double R1_x2 = R1.x + R1.w;
+    double R1_y2 = R1.y - R1.h;
+
+    
+    double R2_x2 = R2.x + R2.w;
+    double R2_y2 = R2.y - R2.h;
+
+    
+    double overlap_w = min(R1_x2, R2_x2) - max(R1.x, R2.x);
+    double overlap_h = min(R1.y, R2.y) - max(R1_y2, R2_y2);
+
+    double overlap_area = max(0.0, overlap_w) * max(0.0, overlap_h);
+    
+    return overlap_area;
+
 
 
 
